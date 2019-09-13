b088aa992c14cc136834550e6652ce356ea06761
laurenchapman <54180127+laurenchapman@users.noreply.github.com>
2019-09-13 10:06:10 -0400

    Create nutz.java

---
+++ nutz.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class madlibs {
+	
+	public static void main(String[] args) {
+		
+		Scanner chris = new Scanner(System.in);
+		
+		String omar = chris.nextLine();
+		
+		
+		
+	System.out.println("Enter a name");
+	
+	String name = chris.nextLine();
+	
+System.out.println("Enter a location");
+	
+	String location = chris.nextLine();
+	
+System.out.println("Enter a noun");
+	
+	String noun = chris.nextLine();
+	
+System.out.println("Enter a bodypart");
+	
+	String bodypart = chris.nextLine();
+	
+System.out.println("Enter a phrase");
+	
+	String phrase = chris.nextLine();
+	
+System.out.println("Enter an adjective");
+	
+	String adjective = chris.nextLine();
+	
+System.out.println("Enter a verb");
+	
+	String verb = chris.nextLine();
+	
+System.out.println("Enter an adjective");
+	
+	String adjective2  = chris.nextLine();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	System.out.println("Mickeys not so scary adventure");
+	System.out.println("Hiya" verb "tonight is the night we are going to have some fun ;). Are you ready to head to" adjective"? Upon arriving at said location, Goofy trips over a $$ and busts his $$ open. After busting his $$ he screams $$. You become $$ and $$ on the spot. Mickeys not so scry adventure, ha. More like a Mickeys not so $$ adventure.");
+	
+	}
+
+}
